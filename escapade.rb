69e0ef7a0bda1471911ac8d5c1a71739975ca67a
vysogot <jakub.godawa@gmail.com>
2022-12-06 16:33:55 +0100

    fix the escapade file

--- escapade.rb
+++ escapade.rb
@@ -1,12 +1,12 @@
 require 'observer'
 
-require './lib/nasa-rover/rover.rb'
-require './lib/nasa-rover/planet.rb'
-require './lib/nasa-rover/x_soft.rb'
-require './lib/nasa-rover/houston.rb'
-require './lib/nasa-rover/orders_reader.rb'
-require './lib/nasa-rover/file_orders_reader.rb'
-require './lib/nasa-rover/string_orders_reader.rb'
+require_relative './lib/nasa_rover/rover.rb'
+require_relative './lib/nasa_rover/planet.rb'
+require_relative './lib/nasa_rover/x_soft.rb'
+require_relative './lib/nasa_rover/houston.rb'
+require_relative './lib/nasa_rover/orders_reader.rb'
+require_relative './lib/nasa_rover/file_orders_reader.rb'
+require_relative './lib/nasa_rover/string_orders_reader.rb'
 
 houston = Houston.new('./test/fixtures/test_input.txt')
 print houston.start_the_mission!
